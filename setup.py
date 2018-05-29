567bfb8e94571164a03196058fd6b98b86120f67
John Paraskevopoulos <io.paraskev@gmail.com>
2018-05-29 17:25:58 +0300

    Bump version: 0.1.1 → 0.2.0

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ test_requirements = [
 
 setup(
     name='cenotes_lib',
-    version='0.1.1',
+    version='0.2.0',
     description="Cenotes libraries",
     long_description=readme + '\n\n' + history,
     author="John Paraskevopoulos",
